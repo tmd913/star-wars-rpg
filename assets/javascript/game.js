0ba009cbd0bf08263d8b5dd319e18943bbb44880
Tom Dennis <tdennis913@gmail.com>
2018-08-25 15:28:24 -0400

    initial html/css

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+// star wars rpg
+
+// game object 
+/*  
+    lightCharacterArray
+    darkCharacterArray
+    currentLightCharacter
+    currentDarkCharacter
+    multiplier
+    currentAttack
+    gameOver
+    methods:
+        changeCharacter
+*/
+
+// character object 
+/*
+    name
+    image
+    hp
+    baseAttack
+    counter
+*/
